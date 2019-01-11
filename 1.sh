8a580091fd38521fd23a0111bb163451bb70bb1a
AntonyWilsonClub <46566514+AntonyWilsonClub@users.noreply.github.com>
2019-01-12 05:24:31 +0800

    Create 1.sh

---
+++ 1.sh
@@ -0,0 +1,43 @@
+echo -e "
+  ${Green_font_prefix}1.${Font_color_suffix} 安装 ShadowsocksR
+  ${Green_font_prefix}2.${Font_color_suffix} 安装 SeverStatus
+  ${Green_font_prefix}3.${Font_color_suffix} 安装 BBR加速
+ "
+	menu_status
+	echo && read -e -p "请输入数字 [1-3]：" num
+case "$num" in
+	1)
+	Install_SSR
+	;;
+	2)
+	Install_SeverStatus
+	;;
+	3)
+	Install_BBR
+	;;
+	*)
+	echo -e "${Error} 请输入正确的数字 [1-3]"
+	;;
+esac
+fi
+
+Install_SSR()
+ {
+  wget https://raw.githubusercontent.com/AntonyWilsonClub/SSR/master/ssr.sh
+  chmod +x ssr.sh
+  bash ssr.sh
+ }
+
+Install_SeverStatus()
+ {
+  wget https://raw.githubusercontent.com/AntonyWilsonClub/SSR-Status/master/ServerStatus.sh
+  chmod +x ServerStatus.sh
+  bash ServerStatus.sh
+ }
+
+Install_BBR()
+ {
+  wget https://raw.githubusercontent.com/AntonyWilsonClub/BBR/master/BBR.sh
+  chmod +x BBR.sh
+  ./BBR.sh
+ }
