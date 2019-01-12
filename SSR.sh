f963820f3fe01448ff0979e536b39dbe2b711975
AntonyWilsonClub <46566514+AntonyWilsonClub@users.noreply.github.com>
2019-01-12 17:02:05 +0800

    Update SSR.sh

--- SSR.sh
+++ SSR.sh
@@ -20,6 +20,10 @@ chmod +x BBR_Install.sh
 ./BBR_Install.sh
 }
 
+Install_BT(){
+yum install -y wget && wget -O install.sh http://download.bt.cn/install/install_6.0.sh && bash install.sh
+}
+
 Uninstall_SSR(){
 ./SSR_Install.sh uninstall
 }
@@ -30,7 +34,8 @@ echo -e "
  ${Green_font_prefix}1.${Font_color_suffix} 安装 SSR
  ${Green_font_prefix}2.${Font_color_suffix} 安装 SeverStatus 
  ${Green_font_prefix}3.${Font_color_suffix} 安装 BBR
- ${Green_font_prefix}4.${Font_color_suffix} 卸载 SSR"
+ ${Green_font_prefix}4.${Font_color_suffix} 安装 宝塔面板
+ ${Green_font_prefix}5.${Font_color_suffix} 卸载 SSR"
  echo
 read -p " 请输入数字 [1-4]:" num
 case "$num" in
@@ -44,6 +49,9 @@ case "$num" in
 	Install_ServerStatus
 	;;
 	4)
+	Install_BT
+	;;
+	5)
 	Uninstall_SSR
 	;;
 	*)
