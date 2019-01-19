a67675e9c90488ce91b6e2d8cccf8f6e62587b10
AntonyWilsonClub <46566514+AntonyWilsonClub@users.noreply.github.com>
2019-01-19 13:36:16 +0800

    Update SSR.sh

--- SSR.sh
+++ SSR.sh
@@ -52,9 +52,9 @@ echo "卸载成功"
 start_menu(){
 clear
 echo -e "
-  一键安装管理脚本 V1.00
- ${Green_font_prefix}1.${Font_color_suffix} BBR   加 速  配置菜单 
- ${Green_font_prefix}2.${Font_color_suffix} 状 态 监 控  配置菜单 
+  一键安装管理脚本 V1.01
+ ${Green_font_prefix}1.${Font_color_suffix} B B R 加 速  菜单 
+ ${Green_font_prefix}2.${Font_color_suffix} 状 态 监 控  菜单 
  ${Green_font_prefix}3.${Font_color_suffix} 一 键 安 装  S S R
  ${Green_font_prefix}4.${Font_color_suffix} 一 键 安 装  宝塔面板
  ${Green_font_prefix}5.${Font_color_suffix} 一 键 卸 载  S S R
