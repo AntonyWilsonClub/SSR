89772d1398f61584e78be812741a4e37204996ec
AntonyWilsonClub <46566514+AntonyWilsonClub@users.noreply.github.com>
2019-01-28 21:10:36 +0800

    Update MENU.sh

--- MENU.sh
+++ MENU.sh
@@ -32,6 +32,38 @@ rm -f install_6.0.sh
 yum install -y wget && wget -O install.sh http://download.bt.cn/install/install_6.0.sh && bash install.sh
 }
 
+#SSR管理
+SSR_MENU(){
+clear
+echo -e "
+ ${Green_font_prefix}1.${Font_color_suffix} 启动  S S R
+ ${Green_font_prefix}2.${Font_color_suffix} 停止  S S R
+ ${Green_font_prefix}3.${Font_color_suffix} 重启  S S R
+ ${Green_font_prefix}4.${Font_color_suffix} SSR  状  态"
+  echo
+read -p " 请输入数字 [1-4]:" num
+case "$num" in
+	1)
+	/etc/init.d/shadowsocks start
+	;;
+	2)
+	/etc/init.d/shadowsocks stop
+	;;
+	3)
+	/etc/init.d/shadowsocks restart
+	;;
+	4)
+	/etc/init.d/shadowsocks status
+	;;
+	*)
+	clear
+	echo -e "请输入正确数字 [1-4]"
+	sleep 3s
+	SSR_MENU
+	;;
+  esac
+}
+
 #卸载脚本
 Uninstall_SH(){
 rm -f MENU.sh
@@ -57,16 +89,17 @@ echo "卸载成功"
 start_menu(){
 clear
 echo -e "
-  一键安装管理脚本 V1.05
+  一键安装管理脚本 V1.15
  ${Green_font_prefix}1.${Font_color_suffix} 一 键 安 装  S S R
  ${Green_font_prefix}2.${Font_color_suffix} 一 键 安 装  宝塔面板 
  ${Green_font_prefix}3.${Font_color_suffix} B B R 加 速  管理菜单
  ${Green_font_prefix}4.${Font_color_suffix} 状 态 监 控  管理菜单
- ${Green_font_prefix}5.${Font_color_suffix} 一 键 卸 载  S S R
- ${Green_font_prefix}6.${Font_color_suffix} 一 键 卸 载  脚本
- ${Green_font_prefix}7.${Font_color_suffix} 退 出 脚 本"
+ ${Green_font_prefix}5.${Font_color_suffix} S  S  R    服务管理
+ ${Green_font_prefix}6.${Font_color_suffix} 一 键 卸 载  S S R
+ ${Green_font_prefix}7.${Font_color_suffix} 一 键 卸 载  脚本
+ ${Green_font_prefix}8.${Font_color_suffix} 退 出 脚 本"
  echo
-read -p " 请输入数字 [1-7]:" num
+read -p " 请输入数字 [1-8]:" num
 case "$num" in
 	1)
 	Install_SSR
@@ -80,18 +113,21 @@ case "$num" in
 	4)
 	Install_ServerStatus
 	;;
-	5)
-	Uninstall_SSR
+	5）
+	SSR_MENU
 	;;
 	6)
-	Uninstall_SH
+	Uninstall_SSR
 	;;
 	7)
+	Uninstall_SH
+	;;
+	8)
 	Exit_SH
 	;;
 	*)
 	clear
-	echo -e "请输入正确数字 [1-7]"
+	echo -e "请输入正确数字 [1-8]"
 	sleep 3s
 	start_menu
 	;;
